9f386b96c0e4591151569a3c88ac09ae3e571fbd
stefanoroque <stefanoroque98@gmail.com>
2021-01-19 16:00:51 -0500

    added layout and started sign in page

--- ticket_tracker/views.py
+++ ticket_tracker/views.py
@@ -1,7 +1,37 @@
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
+from django.utils import timezone
+from django.core.paginator import Paginator
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import *
 
 # Create your views here.
 
 def index(request):
     return render(request, "ticket_tracker/index.html")
+
+def signin_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "ticket_tracket/signin.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "ticket_tracker/signin.html")
